e0ea3ea7a227b67931ed74c3c011324ff1589db9
Yi Zhang <zhangyi_aero@hotmail.com>
2023-04-19 14:50:48 +0200

    jupyter notebooks config

--- web/source/conf.py
+++ web/source/conf.py
@@ -9,12 +9,12 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('./'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'pHlib'
+project = 'PHYEM'
 copyright = '2023, Yi Zhang'
 author = 'Yi Zhang'
 release = '0.0.1'
@@ -28,7 +28,6 @@ extensions = [
 
 templates_path = ['_templates']
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
--- web/source/index.rst
+++ web/source/index.rst
@@ -1,15 +1,24 @@
-.. pHlib documentation master file, created by
+.. PHYEM documentation master file, created by
    sphinx-quickstart on Wed Apr 19 11:03:00 2023.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to pHlib's documentation!
-=================================
+=====
+PHYEM
+=====
+
+Documentation
+=============
+
+Tutorial
+========
+
+Jupyter notebooks
+=================
 
 
 .. toctree::
-   :maxdepth: 2
-   :caption: Contents:
+   :maxdepth: 1
 
    jupyter/test1
 
